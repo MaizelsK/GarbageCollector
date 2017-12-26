985328bc934b9f5a5ac957407d51beed9586f081
Maizels Kirill <domo.ddr@gmail.com>
2017-12-26 21:27:23 +0600

    Выпонил домашнюю работу

--- GarbageCollector/Program.cs
+++ GarbageCollector/Program.cs
@@ -10,6 +10,14 @@ namespace GarbageCollector
     {
         static void Main(string[] args)
         {
+            // Homework
+            User user = new User { Login = "wasd", Password = "123" };
+
+            Console.WriteLine("До изменения поколения: {0}", GC.GetGeneration(user));
+
+            GC.Collect(GC.GetGeneration(user));
+
+            Console.WriteLine("После: {0}", GC.GetGeneration(user));
         }
     }
 }
